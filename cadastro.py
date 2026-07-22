27ceb6cac6f08dc852179bfc09025e6d07d86cd9
FelipeEmmendoerfer <felipe.emmendoerfer@gmail.com>
2026-07-21 22:54:32 -0300

    Modelo base

--- cadastro.py
+++ cadastro.py
@@ -2,6 +2,19 @@ from PySimpleGUI import PySimpleGUI as sg
 
 #Layout
 sg.theme('Reddit')
-
+layout = [
+    [sg.Text('Nome'), sg.Input(key='nome', size=(20,1))],
+    [sg.Text ('Senha'),sg.Input(key='senha', password_char='*')],
+    [sg.Checkbox('Salvar o login?')],
+    [sg.Button('Entrar')]
+]
 #janelas
+janela = sg.Window('Tela de login', layout)
 #Ler cadastro
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['nome'] == 'Felipe' and valores['senha'] == 'Felipe123':
+            print('Bem vindo Felipe')
